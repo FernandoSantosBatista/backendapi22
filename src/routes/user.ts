6b7fe1c2578d6c241eeac301a2608e0e2a9a84bc
FernandodotDev <fernandodossantos005@gmail.com>
2022-04-11 18:02:09 -0300

    '-'

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
-import authenticate from '../middlewares/auth';
 
 const userRoutes = Router();
 const userController = new UserController();
 
 userRoutes.post('/', userController.create);
-userRoutes.patch('/:id', authenticate, userController.enable);
+userRoutes.patch('/:id', userController.enable);
 
 export default userRoutes;
